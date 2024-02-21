c2a7dc50bc284c0b71a4746cafdf00917c672876
omarkhalil117 <omarkhalil117@gmail.com>
2024-02-21 13:03:19 +0200

    build base

--- GoodReads/src/app/app.component.ts
+++ GoodReads/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { AuthorComponent } from '../components/author/author.component';
+import { BookRowComponent } from '../components/book-row/book-row.component';
+import { UserDashboardComponent } from '../components/user-dashboard/user-dashboard.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AuthorComponent , UserDashboardComponent, BookRowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ GoodReads/src/components/book-row/book-row.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-row',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-row.component.html',
+  styleUrl: './book-row.component.css'
+})
+export class BookRowComponent {
+
+}
