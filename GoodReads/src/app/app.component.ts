12913e27ada3a7f86f29dbdab5c5c0262b8e519f
omarkhalil117 <omarkhalil117@gmail.com>
2024-02-21 07:32:03 +0200

    Revert "dubmped commit"
    This reverts commit 0da99a3425a1c66777ef439717916ebdd16dfb32.

--- GoodReads/src/app/app.component.ts
+++ GoodReads/src/app/app.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , FormsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title:string = 'GoodReads';
-  Name:string = 'omar';
-  result:number = 0;
-  input1 : string = '';
-  input2 : string = '';
-  add() : void
-  {
-      this.result = parseInt(this.input1) + parseInt(this.input2)  ;
-      console.log(this.result);
-  }
+  title = 'GoodReads';
 }
