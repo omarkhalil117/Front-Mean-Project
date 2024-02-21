0da99a3425a1c66777ef439717916ebdd16dfb32
omarkhalil117 <omarkhalil117@gmail.com>
2024-02-21 07:26:52 +0200

    dubmped commit

--- GoodReads/src/app/app.component.ts
+++ GoodReads/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'GoodReads';
+  title:string = 'GoodReads';
+  Name:string = 'omar';
+  result:number = 0;
+  input1 : string = '';
+  input2 : string = '';
+  add() : void
+  {
+      this.result = parseInt(this.input1) + parseInt(this.input2)  ;
+      console.log(this.result);
+  }
 }
---
+++ GoodReads/src/app/app.modules.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule  } from '@angular/core';
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule ,
+    BrowserModule,
+  ],
+  providers: [],
+})
+export class AppModule { }
