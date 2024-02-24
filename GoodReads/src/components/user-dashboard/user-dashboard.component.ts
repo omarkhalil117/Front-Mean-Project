48e800630b85fea2088df48840a66e98b2b0a117
omarkhalil117 <omarkhalil117@gmail.com>
2024-02-25 00:02:47 +0200

    add filter by shelve state

--- GoodReads/src/components/user-dashboard/user-dashboard.component.ts
+++ GoodReads/src/components/user-dashboard/user-dashboard.component.ts
@@ -10,15 +10,40 @@ import { UserDataService } from '../../services/user-data.service';
 })
 export class UserDashboardComponent {
   data:any;
+  books:any;
+  temp:any;
   userToken:String = '';
   constructor(private userData : UserDataService){}  
 
   ngOnInit()
   {
-    this.userData.getUserInfo('65d8f826d9f7850808bdd6ad').subscribe((data) => {
+    this.userData.getUserInfo('65d2e73c85d0e459ad9f7c3b').subscribe((data) => {
       this.data= data
+      this.books = this.data.fullInfo.books
+      this.temp = this.books
       console.log(this.data)
     });
+  }
+
+  getAll()
+  {
+    this.books = this.temp;
+  }
+  getReadBook()
+  {
+    this.books = this.temp.filter((el:any)=> el.shelve === "Read")
+    console.log(this.books)
+  }
+
+  getWantToReadBooks()
+  {
+    this.books = this.temp.filter((el:any)=> el.shelve === "Want to Read")
+    console.log(this.books)
+  }
+  getReadingBooks()
+  {
+    this.books = this.temp.filter((el:any)=> el.shelve === "Currently Read")
+    console.log(this.books)
   }
 
 
