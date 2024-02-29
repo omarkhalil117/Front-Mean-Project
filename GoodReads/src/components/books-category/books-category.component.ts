fad0057a87e3fed79f6b3f3d30c57fae9a9f0c94
EngAlaaKamal <alaa.kamal.ghonem@gmail.com>
2024-02-29 21:32:03 +0200

    last front

--- GoodReads/src/components/books-category/books-category.component.ts
+++ GoodReads/src/components/books-category/books-category.component.ts
@@ -2,11 +2,11 @@
 import { Component } from '@angular/core';
 import { Books } from '../../models/books';
 import { Book } from '../../models/book';
-import { ActivatedRoute } from '@angular/router';
 import { RouterLink } from '@angular/router';
-import { Router } from '@angular/router';
 import { Category } from '../../models/category';
 import {CategoriesApiService } from '../../services/categories-api.service';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-books-category',
@@ -23,58 +23,36 @@ export class BooksCategoryComponent {
   page !:any;
 
 
-  constructor(private activateRoute: ActivatedRoute,private categoriesRequests: CategoriesApiService,private router : Router) {}
-//hold id param from route using activaterouter
-  ngOnInit(){
-   // this.activateRoute.paramMap.subscribe({
-   //  next:(params)=>{
-   //     const id = params.get('categoryId');
-   //   console.log('Route parameter id:',id);
-  //   }
-   // });
+  constructor(private activateRoute: ActivatedRoute,private categoriesRequests: CategoriesApiService,private router : Router) {
 
-    const idfromroute = this.activateRoute.snapshot.params['categoryId'];
-    console.log(idfromroute)
-    this.categoriesRequests.getBooksByCategoryId(idfromroute).subscribe((res : any) => {this.books = res.booksbycategory; this.categoryname=res.categorycontent.name; console.log(this.books)});
-//i need make api-service to get realybook
+ this.router.events.pipe(
+  filter(event => event instanceof NavigationEnd)
+).subscribe(() => {
+  this.loadBooks();
+});
+  }
 
-this.loadBooks();
-}
-
-
+  ngOnInit(){}
 
 loadBooks() {
   const idfromroute = this.activateRoute.snapshot.params['categoryId'];
   this.page = this.activateRoute.snapshot.queryParamMap.get('page');
   this.categoriesRequests.getBooks(this.page ,idfromroute)
   .subscribe((res : any) => {this.arr = res.booksbycategory; this.categoryname=res.categorycontent.name; console.log(res)});
- // .subscribe((res:any) =>{console.log(res);  this.arr = res});
+
 }
 
 nextPage(): void {
   let page = this.page;
   page++;
+  this.page = page;
   console.log(page);
   const idfromroute = this.activateRoute.snapshot.params['categoryId'];
   this.router.navigate(['categories', idfromroute], { queryParams:{page} });
-  this.loadBooks();
+
 }
 
-//clickPage(e:any)
-//{
- // console.log(e.target.innerHTML);
 
- // let page = this.page;
- // if(e.target.innerHTML=='Previous'){
- //   page--;
- // }else if(e.target.innerHTML=='Next'){
- //   page++;
- // }
- // console.log(page);
- // const idfromroute = this.activateRoute.snapshot.params['categoryId'];
-//  this.router.navigate(['categories', idfromroute], { queryParams:{page} });
-//  this.loadBooks();
-//}
 
 prevPage(): void {
   if (this.page > 1) {
@@ -83,7 +61,7 @@ prevPage(): void {
    console.log(page);
     const idfromroute = this.activateRoute.snapshot.params['categoryId'];
     this.router.navigate(['categories', idfromroute], { queryParams:{page} });
-    this.loadBooks();
+
   }
 }
 
